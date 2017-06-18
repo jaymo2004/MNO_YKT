9508dcb4d858689db75b14699500975b0695a6e2
boluo123456 <guochunhao@163.com>
2017-06-18 22:10:12 +0800

    Update randomPassword.py

--- randomPassword.py
+++ randomPassword.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 import random
 
-seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-"
-seed2 = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-sa = []
+setAll = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-"
+setNumChar = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+passwordArray = []
 
 print ("1.只包含大小写字母")
 print ("2.包含大小写字母与特殊符号")
@@ -11,13 +11,13 @@ passType = input("请选择密码类型(选择1 或 2)：")
 length=input("请输入所需密码的位数： ")
 if passType == 1:
     for i in range(length):
-        sa.append(random.choice(seed2))
-    salt = ''.join(sa)
+        passwordArray.append(random.choice(setNumChar))
+    salt = ''.join(passwordArray)
     print salt
 elif passType == 2:
     for i in range(length):
-        sa.append(random.choice(seed))
-    salt = ''.join(sa)
+        passwordArray.append(random.choice(setAll))
+    salt = ''.join(passwordArray)
     print salt
 else:
     print ("密码类型选择错误")
